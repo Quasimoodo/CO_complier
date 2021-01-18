45540ee4040d89c640199d094740c888470cca89
Quasimoodo <63183315+Quasimoodo@users.noreply.github.com>
2021-01-18 10:30:22 +0800

    Update App.java

--- src/compiler/App.java
+++ src/compiler/App.java
@@ -1,4 +1,8 @@
 package compiler;
+
+import java.io.File;
+import java.util.Scanner;
+
 import compiler.analyzer.*;
 import compiler.tokenizer.*;
 
@@ -6,7 +10,9 @@ public class App
 {
     public static void main(String[] args) throws Exception
     {
-        Tokenizer ter =new Tokenizer(args[0]);
+        Scanner sc = new Scanner(new File(args[0]));
+        StringIter it = new StringIter(sc);
+        Tokenizer tokenizer = new Tokenizer(it);
       //  Objectfile obj=new 
      //   Analyzer aer = new Analyzer(ter);
     /*    try
